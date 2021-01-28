8c8fc70d0f705ce112a1f9617b3256f3ca3982b8
segural <luciano.g.segura@gmail.com>
2021-01-28 09:24:05 -0300

    armado de estructura y ordené en carpetas archivos

---
+++ app.js
@@ -0,0 +1,17 @@
+//requiero paquetes y genero las variables(espress,path)
+const express = require ('express');
+const path = require ('path');
+const app = express ();
+const publicPath = path.resolve ('public');
+
+app.use(express.static (publicPath));
+
+//hago la peticion al server de las htmls
+app.get ('/', (req,res) =>{
+    res.sendFile (path.resolve ('views/home.html'))
+})
+
+//levanto server express en puerto 8000
+app.listen (8000, () => {
+    console.log ('Servidor corriendo en puerto 8000');
+})

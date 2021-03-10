c4a1c0970b93d06f178452d4d7120e229f7b6550
efesalinas <efesalinas@hotmail.com>
2021-03-10 00:23:05 -0300

    Arreglos de URL

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,10 +1,11 @@
 // Defino en cada método del controlador cuál será la respuesta a cada requerimiento
 const usersController ={
-    index: (req, res)=>{ res.render ('home')},
+    index: (req, res)=>{ res.render ('./users/register')},
 
     login: (req, res)=>{ res.render ('./users/login')},
 
-    register: (req,res) =>{res.render ('./users/register')},
+    // register: armar método POST register
+
     
 }
 

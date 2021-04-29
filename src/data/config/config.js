7adb537bae7bebbefa09c045371f09da071d69e8
segural <luciano.g.segura@gmail.com>
2021-04-29 19:09:48 -0300

    arreglo de archivo config

--- src/data/config/config.js
+++ src/data/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
     "development": {
       "username": "root",
       "password": 'root',
-      "database": "casa_digital_bd",
+      "database": "casa_digital_db",
       "host": "127.0.0.1",
       "dialect": "mysql"
     },

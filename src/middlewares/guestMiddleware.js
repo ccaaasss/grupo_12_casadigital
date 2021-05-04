c79042b751f27f6c9b1d2b28b2d992c45bdcc584
efesalinas <efesalinas@hotmail.com>
2021-05-04 17:04:07 -0300

    Ultimas correcciones al S6

--- src/middlewares/guestMiddleware.js
+++ src/middlewares/guestMiddleware.js
@@ -5,7 +5,7 @@ function guestMiddleware (req, res, next){
     if(req.session.userLogged == undefined){
         next();
     } else{
-        // res.redirect("./users/userProfile")
+        res.redirect("/users/userProfile")
     };
 
 };

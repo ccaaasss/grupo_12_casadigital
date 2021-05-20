4ea771a05e76b16dd2e40e7233db053e5a8447c3
efesalinas <efesalinas@hotmail.com>
2021-05-20 00:52:35 -0300

    Validaciónes backend usuarios

--- src/routes/users.js
+++ src/routes/users.js
@@ -27,16 +27,56 @@ const upload = multer({storage: storage});
 
 /* Validaciones */
 const {check} = require("express-validator");
-let validateLogin = [
-    check("email").isEmail().withMessage("Email invalido"),
-    check("password").isLength({min: 8}).withMessage("la contraseña debe tener al menos 8 caracteres")
+let validateUserLogin = [
+    check("email")
+        .notEmpty().withMessage("Debes introducir un email").bail()
+        .isEmail().withMessage("Email invalido. Ingresa un email con formato válido: user@dominio.com").bail(),
+        
+    check("password")
+        .isLength({min: 8}).withMessage("La contraseña debe tener entre 8 y 15 caracteres").bail()
+        .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/).withMessage("La contraseña debe incluir al menos una minúscula, una mayúscula, un número y un caracter especial")
+];
+let validateUserStore = [
+    check("first_name")
+        .notEmpty().withMessage("Debes introducir un nombre").bail()
+        .isLength({min: 2}).withMessage("El nombre debe tener al menos 2 caracteres"),
+
+    check("last_name")
+        .notEmpty().withMessage("Debes introducir un apellido").bail()
+        .isLength({min: 2}).withMessage("El apellido debe tener al menos 2 caracteres"),
+
+    check("email")
+        .notEmpty().withMessage("Debes introducir un email").bail()
+        .isEmail().withMessage("Email invalido. Ingresa un email con formato válido: user@dominio.com"),
+
+    check("password")
+        .isLength({min: 8}).withMessage("La contraseña debe tener entre 8 y 15 caracteres").bail()
+        .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/).withMessage("La contraseña debe incluir al menos una minúscula, una mayúscula, un número y un caracter especial"),
+
+    check("birth_date")
+        .notEmpty().withMessage("Debes introducir tu fecha de nacimiento").bail(),
+
+    check("image").custom((value, {req}) =>{
+        switch(path.extname(req.file.originalname)){
+            case ".jpg": return ".jpg";
+            break;
+            case ".jpeg": return ".jpeg";
+            break;
+            case ".png": return ".png";
+            break;
+            case ".gif": return ".gif";
+            break;
+
+            default: return false
+        }
+    }).withMessage("Solo se admiten imágenes .jpg, .jpeg, .png, .gif")
 ];
 
 // Defino las rutas, es decir que controlador y cuál de sus métodos es el que va a manejar el requerimiento
 
 // Creación de usuarios 
 router.get("/register", guestMiddleware, usersController.register);
-router.post('/', upload.single("image") , usersController.store);
+router.post('/', upload.single("image"), validateUserStore, usersController.store);
 
 // Perfil de usuarios
 router.get("/userProfile", usersController.userProfile);
@@ -44,7 +84,7 @@ router.get("/userProfile", usersController.userProfile);
 // Proceso de formulario de Login
 
 router.get("/login", guestMiddleware, usersController.login);
-router.post("/login", validateLogin, usersController.loginProcess);
+router.post("/login", validateUserLogin, usersController.loginProcess);
 router.post("/logout", usersController.logoutProcess);
 
 module.exports = router;
